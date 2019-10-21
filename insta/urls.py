5bae4bcdeaf5b331fc0bdce5673c2574e7465c5b
Naume-k <kobnaome@gmail.com>
2019-10-21 15:36:16 +0200

    added the urls of the app

---
+++ insta/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+    url(r'^new/upload$', views.upload_image, name='upload'),
+    url(r'^new/profile$', views.add_profile, name='edit'),
+    url(r'^myprofile$', views.my_profile, name='myprofile'),
+    url(r'^comment/(\d+)/$', views.add_comment, name='comment'),
+    url(r'^search/',views.search_users, name = 'searchs'),
+    
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
